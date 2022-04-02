7a3803a201918a04bf08e4d8d3c27113986b1171
ivangarcia44 <36650061+ivangarcia44@users.noreply.github.com>
2022-04-02 17:17:39 -0400

    Add files via upload

--- downloadSemSegNet.m
+++ downloadSemSegNet.m
@@ -9,8 +9,24 @@ else
     try
         dataFolder = fullfile(pwd,"coco");
         trainedMaskRCNN_url = "https://www.mathworks.com/supportfiles/vision/data/maskrcnn_object_person_car_v2.mat";
-        helper.downloadTrainedMaskRCNN(trainedMaskRCNN_url,dataFolder);
-        pretrained = load(fullfile(dataFolder,"maskrcnn_object_person_car.mat"));
+
+        filename = 'maskrcnn_pretrained_person_car.mat';
+        netDirFullPath = dataFolder;
+        netFileFullPath = fullfile(dataFolder,filename);
+
+        if ~exist(netFileFullPath,'file')
+            fprintf('Downloading pretrained MaskRCNN network.\n');
+            fprintf('This can take several minutes to download...\n');
+            if ~exist(netDirFullPath,'dir')
+                mkdir(netDirFullPath);
+            end
+            websave(netFileFullPath,trainedMaskRCNN_url);
+            fprintf('Done.\n\n');
+        else
+            fprintf('Pretrained MaskRCNN network already exists.\n\n');
+        end
+        pretrained = load(fullfile(dataFolder,"maskrcnn_pretrained_person_car.mat"));
+
         semSegNet = pretrained.net;
         
         save(semSegFileName, 'semSegNet');
